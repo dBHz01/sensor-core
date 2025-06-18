293c2d167f1470921ae2d7df6f27635c403a1b26
dBHz <928300456@qq.com>
2025-06-18 17:37:33 +0800

    feat(ring): Add audio related.

--- ring/ble_ring_v2.py
+++ ring/ble_ring_v2.py
@@ -10,6 +10,7 @@ from typing import Tuple
 
 import sys
 import os
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "."))
 from utils.imu_data import IMUData
 
@@ -27,25 +28,37 @@ class NotifyProtocol:
     CALIB_IMU = bytearray([0x00, 0x00, 0x40, 0x02])
     CALIB_TIME = bytearray([0x00, 0x00, 0x99, 0x00])
     GET_TOUCH = bytearray([0x00, 0x00, 0x61, 0x00])
-    OPEN_AUDIO = bytearray([0x00, 0x00, 0x71, 0x00, 0x01])
-    CLOSE_AUDIO = bytearray([0x00, 0x00, 0x71, 0x00, 0x00])
+    OPEN_MIC  = bytearray([0x00, 0x00, 0x71, 0x01, 0x01])
+    CLOSE_MIC = bytearray([0x00, 0x00, 0x71, 0x00])
+  
     GET_NFC = bytearray([0x00, 0x00, 0x82, 0x00])
 
 
 class BLERing:
 
-    def __init__(self, address:str, index:int, gyro_bias:Tuple[float]=None, imu_callback=None,
-        battery_callback=None, touch_callback=None, imu_freq=200.0):
+    def __init__(
+        self,
+        address: str,
+        index: int,
+        gyro_bias: Tuple[float] = None,
+        imu_callback=None,
+        battery_callback=None,
+        touch_callback=None,
+        audio_callback=None,
+        imu_freq=200.0,
+    ):
         self.address = address
         self.index = index
         if gyro_bias is None:
             self.gyro_bias = (0, 0, 0)
-        else: self.gyro_bias = gyro_bias
+        else:
+            self.gyro_bias = gyro_bias
         self.imu_mode = False
         self.raw_imu_data = bytearray()
         self.touch_callback = touch_callback
         self.battery_callback = battery_callback
         self.imu_callback = imu_callback
+        self.audio_callback = audio_callback
         self.acc_fsr = "0"
         self.gyro_fsr = "0"
         self.imu_freq = imu_freq
@@ -69,11 +82,9 @@ class BLERing:
         self.end_calib_timestamps = []
         self.start_calib_timestamps = []
 
-
     @property
     def name(self):
         return "Ring" + str(self.index)
-
 
     def on_disconnect(self, clients):
         # print('Disconnected')
@@ -85,7 +96,7 @@ class BLERing:
             if self.taped:
                 if counter != -1:
                     if self.touch_callback is not None:
-                        self.touch_callback(self.index, 1) # double-tap
+                        self.touch_callback(self.index, 1)  # double-tap
                     counter = -1
                 else:
                     counter = 0
@@ -93,12 +104,14 @@ class BLERing:
             elif counter >= 0:
                 counter += 1
                 if counter == 100:  # tap interval
-                    if self.touch_type == 0: pass
-                    elif self.touch_type == 1: pass
-                    elif self.touch_type == 2: pass
+                    if self.touch_type == 0:
+                        pass
+                    elif self.touch_type == 1:
+                        pass
+                    elif self.touch_type == 2:
+                        pass
                     counter = -1
             time.sleep(0.01)
-
 
     def get_touch_state(self, x, y, z):
         # 0: 000 -> 0
@@ -111,8 +124,7 @@ class BLERing:
         # 7: 111 -> -2
         # print(x, y, z)
         return [0, 1, 3, 2, 5, -1, 4, -2][x * 4 + y * 2 + z]
-    
-    
+
     def _detect_touch_events(self, data: bytearray) -> None:
         new_touch = [
             self.get_touch_state(
@@ -126,23 +138,28 @@ class BLERing:
         if new_touch[0] == 0:
             if not self.is_holding and len(self.touch_history) > 1:
                 self.taped = True
-                if self.touch_history[-2][0] > self.touch_history[0][0]: # down
+                if self.touch_history[-2][0] > self.touch_history[0][0]:  # down
                     self.touch_type = 1
-                elif self.touch_history[-2][0] < self.touch_history[0][0]: # up
+                elif self.touch_history[-2][0] < self.touch_history[0][0]:  # up
                     self.touch_type = 2
-                elif self.touch_history[-2][-1] - self.touch_history[0][-1] < 0.1: # tap
+                elif (
+                    self.touch_history[-2][-1] - self.touch_history[0][-1] < 0.1
+                ):  # tap
                     self.touch_type = 0
                 else:
                     self.touch_type = 0
             elif self.is_holding:
                 if self.touch_callback is not None:
-                    self.touch_callback(self.index, 5) # release
+                    self.touch_callback(self.index, 5)  # release
             self.is_holding = False
             self.touch_history.clear()
         else:
-            if self.touch_history[-1][-1] - self.touch_history[0][-1] > 1 and not self.is_holding:
+            if (
+                self.touch_history[-1][-1] - self.touch_history[0][-1] > 1
+                and not self.is_holding
+            ):
                 if self.touch_callback is not None:
-                    self.touch_callback(self.index, 2) # long-touch
+                    self.touch_callback(self.index, 2)  # long-touch
                 self.is_holding = True
 
     def _detect_double_tap_with_tap(self):
@@ -150,32 +167,31 @@ class BLERing:
         if time.perf_counter() - self.last_tap_time < 0.5:
             double_tapped = True
             if self.touch_callback is not None:
-                self.touch_callback(self.index, 1) # double-tap
+                self.touch_callback(self.index, 1)  # double-tap
         self.last_tap_time = time.perf_counter()
         if double_tapped:
             self.last_tap_time = 0
-
 
     def notify_callback(self, sender, data: bytearray):
         # print(len(data), data)
         bias = self.gyro_bias
         if data[2] == 0x62 and data[3] == 0x1:
-            print('呼吸灯结果')
+            print("呼吸灯结果")
         if data[2] == 0x62 and data[3] == 0x2:
-            print('自定义灯结果')
+            print("自定义灯结果")
         if data[2] == 0x62 and data[3] == 0x3:
-            print('自定义灯pwm空闲结果')
+            print("自定义灯pwm空闲结果")
 
         if data[2] == 0x10 and data[3] == 0x0:
             print(data[4])
         if data[2] == 0x11 and data[3] == 0x0:
-            print('Software version:', data[4:])
+            print("Software version:", data[4:])
         if data[2] == 0x11 and data[3] == 0x1:
-            print('Hardware version:', data[4:])
+            print("Hardware version:", data[4:])
 
         if data[2] == 0x40 and data[3] == 0x06:
-            acc_scale = 32768/16 * (2 ** ((data[4] >> 2) & 3)) / 9.8
-            gyr_scale = 32768/2000 * (2 ** (data[4] & 3)) / (math.pi / 180)
+            acc_scale = 32768 / 16 * (2 ** ((data[4] >> 2) & 3)) / 9.8
+            gyr_scale = 32768 / 2000 * (2 ** (data[4] & 3)) / (math.pi / 180)
             if len(data) > 20:
                 imu_data = []
                 head_length = 4 + len(data) % 2
@@ -189,27 +205,44 @@ class BLERing:
                     imu_packet_num = (len(data) - head_length - 8) // 12
                 else:
                     imu_packet_num = (len(data) - head_length) // 12
-                
+
                 for i in range(head_length, len(data), 12):
                     if len(data) - i < 12:
                         break
-                    acc_x, acc_y, acc_z = struct.unpack("hhh", data[i:i+6])
-                    acc_x, acc_y, acc_z = acc_x / acc_scale, acc_y / acc_scale, acc_z / acc_scale
-                    gyr_x, gyr_y, gyr_z = struct.unpack("hhh", data[i+6:i+12])
-                    gyr_x, gyr_y, gyr_z = gyr_x / gyr_scale, gyr_y / gyr_scale, gyr_z / gyr_scale,
+                    acc_x, acc_y, acc_z = struct.unpack("hhh", data[i : i + 6])
+                    acc_x, acc_y, acc_z = (
+                        acc_x / acc_scale,
+                        acc_y / acc_scale,
+                        acc_z / acc_scale,
+                    )
+                    gyr_x, gyr_y, gyr_z = struct.unpack("hhh", data[i + 6 : i + 12])
+                    gyr_x, gyr_y, gyr_z = (
+                        gyr_x / gyr_scale,
+                        gyr_y / gyr_scale,
+                        gyr_z / gyr_scale,
+                    )
                     if imu_start_time != 0 and imu_end_time != 0:
-                        timestamp = imu_start_time + ((imu_end_time - imu_start_time) / (imu_packet_num - 1)) * ((i - head_length) // 12)
+                        timestamp = imu_start_time + (
+                            (imu_end_time - imu_start_time) / (imu_packet_num - 1)
+                        ) * ((i - head_length) // 12)
                     else:
                         timestamp = time.perf_counter()
-                    imu = IMUData(-1 * acc_y, acc_z, -1 * acc_x,
-                        -1 * gyr_y - bias[0], gyr_z - bias[1], -1 * gyr_x - bias[2], timestamp)
+                    imu = IMUData(
+                        -1 * acc_y,
+                        acc_z,
+                        -1 * acc_x,
+                        -1 * gyr_y - bias[0],
+                        gyr_z - bias[1],
+                        -1 * gyr_x - bias[2],
+                        timestamp,
+                    )
                     imu_data.append(imu)
-                
+
                 # IMU callback function
                 if self.imu_callback is not None:
                     for i, imu in enumerate(imu_data):
                         self.imu_callback(self.index, imu)
-                        
+
         elif data[2] == 0x61 and data[3] == 0x0:
             pass
 
@@ -235,6 +268,17 @@ class BLERing:
             self.end_calib_timestamps.append(time.perf_counter())
             self.ring_timestamps.append(struct.unpack("i", data[4:])[0] / 16384)
 
+        elif data[2] == 0x71 and data[3] == 0x0:
+            audio_type = 0
+            audio = data[-200:]
+            if self.audio_callback is not None:
+                self.audio_callback(self.index, audio_type, audio)
+
+        elif data[2] == 0x71 and data[3] == 0x1:
+            audio_type = 1
+            audio = data[-200:]
+            if self.audio_callback is not None:
+                self.audio_callback(self.index, audio_type, audio)
 
     async def connect(self, callback: FunctionType = None):
         self.client = BleakClient(
@@ -264,7 +308,7 @@ class BLERing:
             print("Connected")
             self.connected = True
             if callback is not None:
-            #   callback(self.index)
+                #   callback(self.index)
                 callback()
 
         while True:
@@ -274,11 +318,9 @@ class BLERing:
                 break
             await asyncio.sleep(2.0)
 
-    
     async def send_command(self, command: bytearray):
         await self.client.write_gatt_char(NotifyProtocol.WRITE_CHARACTERISTIC, command)
         await asyncio.sleep(0.1)
-
 
     async def calibrate_imu(self):
         await self.send_command(NotifyProtocol.CALIB_IMU)
@@ -286,19 +328,35 @@ class BLERing:
         await asyncio.sleep(3.0)
         print("Calibration done")
 
+    async def open_audio(self):
+        await self.send_command(NotifyProtocol.OPEN_MIC)
+        print("Audio opened")
     
+    async def close_audio(self):
+        await self.send_command(NotifyProtocol.CLOSE_MIC)
+        print("Audio closed")
+
+    async def open_imu(self):
+        await self.send_command(NotifyProtocol.OPEN_6AXIS_IMU)
+        print("IMU opened")
+    
+    async def close_imu(self):
+        await self.send_command(NotifyProtocol.CLOSE_6AXIS_IMU)
+        print("IMU closed")
+
     async def calib_time(self):
         start_time = time.perf_counter()
-        await self.client.write_gatt_char(NotifyProtocol.WRITE_CHARACTERISTIC, NotifyProtocol.CALIB_TIME)
+        await self.client.write_gatt_char(
+            NotifyProtocol.WRITE_CHARACTERISTIC, NotifyProtocol.CALIB_TIME
+        )
         end_time = time.perf_counter()
         self.start_calib_timestamps.append((start_time + end_time) / 2)
-
 
     async def disconnect(self):
         await self.client.stop_notify(NotifyProtocol.READ_CHARACTERISTIC)
         await self.client.disconnect()
         print(f"Disconnected from {self.address}")
-    
+
 
 def imu_callback(name, data):
     print(f"[{name}]: {data}")
@@ -330,7 +388,7 @@ async def connect():
     print(ring_macs)
 
     coroutines = []
-    ring_addr = f'7ED722A8-D972-4C34-0CF9-B9059A4BCAF2'
+    ring_addr = f"7ED722A8-D972-4C34-0CF9-B9059A4BCAF2"
     ring = BLERing(ring_addr, index=0, imu_callback=imu_callback)
     coroutines.append(ring.connect())
 
